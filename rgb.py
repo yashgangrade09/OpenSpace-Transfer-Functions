69d813d905619e90a2e9eaf00ce2e502023e8d43
Spencer <spence521@yahoo.com>
2018-03-31 14:05:25 -0600

    updated rgb.py

--- rgb.py
+++ rgb.py
@@ -12,6 +12,11 @@ def pil_test():
     cm_hot = mpl.cm.get_cmap('hot')
     cm_purples = mpl.cm.get_cmap('Purples')
     cm_cool = mpl.cm.get_cmap('cool')
+    cm_spring = mpl.cm.get_cmap('spring')
+    cm_summer = mpl.cm.get_cmap('summer')
+    cm_autumn = mpl.cm.get_cmap('autumn')
+    cm_winter = mpl.cm.get_cmap('winter')
+
 
     img_src = Image.open('testimgGray.png').convert('L')
     img_src.thumbnail((512, 512))
@@ -19,11 +24,20 @@ def pil_test():
 
     im2 = cm_purples(im)
     im3 = cm_cool(im)
-    im = cm_hot(im)
+    im4 = cm_hot(im)
+    im5 = cm_spring(im)
+    im6 = cm_summer(im)
+    im7 = cm_autumn(im)
+    im8 = cm_winter(im)
 
-    save_Image(im, 'test_hot.png')
+    save_Image(im4, 'test_hot.png')
     save_Image(im2, 'test_purple.png')
-    save_Image(im3, 'cool.png')
+    save_Image(im3, 'test_cool.png')
+    save_Image(im5, 'test_spring.png')
+    save_Image(im6, 'test_summer.png')
+    save_Image(im7, 'test_autumn.png')
+    save_Image(im8, 'test_winter.png')
+
 
 def save_Image(im, image_name):
     im = np.uint8(im * 255)
