6fc5136ae53b6810f2f4b4b536eab9cc785eb3e4
MarioMag <mariomaganagarcia@gmail.com>
2018-04-11 14:11:09 -0600

    PixelMan ve2 Apr11

--- ColorMaps.py
+++ ColorMaps.py
@@ -1,6 +1,6 @@
 import cv2
    
-im = cv2.imread("convImg.png")  
-im_color = cv2.applyColorMap(im, cv2.COLORMAP_RAINBOW)
+im = cv2.imread("day_temperature_2018-03-23.png")  
+im_color = cv2.applyColorMap(im, cv2.COLORMAP_HOT)
 
-cv2.imwrite('testimgColor3.png',im_color)
+cv2.imwrite('testimgColor4.png',im_color)
--- transferImage.py
+++ transferImage.py
@@ -4,35 +4,39 @@ import math
 from numpy import linalg as LA
 
 #Search for Pixel In Legend
-def searchPixel(lg, curPix):
-    if curPix[0] <= 20 and curPix[1] <= 20 and curPix[2] <= 20:
+def searchPixel(lg, curPix, dim_legend):
+    if curPix[0] <= 25 and curPix[1] <= 25 and curPix[2] <= 25:
         return 0
     
     bestindex = 0
-    best = LA.norm(np.array([[ int(lg[0][0][0])-int(curPix[0]), int(lg[0][0][1])-int(curPix[1]), int(lg[0][0][2])-int(curPix[2])   ]] ))
+    best = LA.norm(np.array([[ int(lg[0][0][0])-int(curPix[0]), int(lg[0][0][1])-int(curPix[1]), int(lg[0][0][2])-int(curPix[2])   ]] ),np.inf)
    
-    for x in range(235):
+    for x in range(dim_legend):
         if lg[0][x][0] == curPix[0] and lg[0][x][1] == curPix[1] and lg[0][x][2] == curPix[2]:
-            return math.floor(x / 235 * 255)
+            return math.floor(x / dim_legend * 255)
         
-        bestTest = LA.norm(np.array([[ int(lg[0][x][0])-int(curPix[0]), int(lg[0][x][1])-int(curPix[1]), int(lg[0][x][2])-int(curPix[2])   ]] ))
+        bestTest = LA.norm(np.array([[ int(lg[0][x][0])-int(curPix[0]), int(lg[0][x][1])-int(curPix[1]), int(lg[0][x][2])-int(curPix[2])   ]] ),np.inf)
         if best > bestTest :
             bestindex = x
             best = bestTest
                 
-    return math.floor((bestindex / 235) * 255)
+    return math.floor((bestindex / dim_legend) * 255)
 
     
 #Reading In Files
 srclg = cv2.imread("color_bar_legendv2.png")
 srcImg = cv2.imread("day_temperature_2018-03-23.png")
 
+dim_legend = srclg.shape[1]
+row_input = srcImg.shape[0]
+col_input = srcImg.shape[1]
+
 #Loop Over Pixels
 conImg = []
-for i in range(250):
+for i in range(row_input):
     conImg.append([])
-    for j in range(250):
-        conImg[i].append( searchPixel(srclg, srcImg[i][j]) )      
+    for j in range(col_input):
+        conImg[i].append( searchPixel(srclg, srcImg[i][j], dim_legend) )      
         
 conImg = np.asarray(conImg)  
 cv2.imwrite('convImg.png', conImg)
