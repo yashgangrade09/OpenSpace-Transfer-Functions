f23dba5d91df9ee67f8606ec96c36979b43634bc
MarioMag <mariomaganagarcia@gmail.com>
2018-04-16 21:41:07 -0600

    images Entire and Africa

--- transferImage.py
+++ transferImage.py
@@ -25,7 +25,7 @@ def searchPixel(lg, curPix, dim_legend):
     
 #Reading In Files
 srclg = cv2.imread("color_bar_legendv2.png")
-srcImg = cv2.imread("EntireWorld_Temperature_Day.png")
+srcImg = cv2.imread("Africa_day_temperature_2018-03-23.png")
 
 dim_legend = srclg.shape[1]
 row_input = srcImg.shape[0]
@@ -37,30 +37,12 @@ for i in range(row_input):
     print(i)
     conImg.append([])
     for j in range(col_input):
-        conImg[i].append( searchPixel(srclg, srcImg[i][j], dim_legend) )      
+        ## True Approximation
+        #conImg[i].append( searchPixel(srclg, srcImg[i][j], dim_legend) )   
+        
+        ## Lumonsity Method
+        #conImg[i].append(int( round(srcImg[i][j][0]*0.21 + srcImg[i][j][1]*0.72 + srcImg[i][j][2]*0.07)) )   
         
 conImg = np.asarray(conImg)  
-cv2.imwrite('EntireWorldConverted.png', conImg)
+cv2.imwrite('Africa_day_temperature_2018-03-23_True.png', conImg)
 print("Done")
-
-
-
-#===========================OLD CODE============================
-## Reading In Files
-#srclg = cv2.imread("color_bar_legend.png")
-#srcImg = cv2.imread("day_temperature_2018-03-23.png")
-#
-#conImg = []
-#for i in range(864):
-#    conImg.append([])
-#    for j in range(848):
-#        conImg[i].append(int( round(srcImg[i][j][0]*0.21 + srcImg[i][j][1]*0.72 + srcImg[i][j][2]*0.07)) )
-#        
-#conImg = np.asarray(conImg)        
-#
-#
-##plt.imshow(conImg, cmap='gray')
-##plt.savefig('convImg.png')
-#
-#
-#cv2.imwrite('convImg.png', conImg)
